c51267f7032f2ebb71a0f5605681942e70af3575
vabarano <Vasily.Baranovsky@yandex.ru>
2022-02-10 14:43:50 +0100

    Chapter 4. Spring MVC. Lection 60 - аннотация @RequestParam

--- spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package com.zaurtregulov.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,9 +25,19 @@ public class MyController {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//        String empName=request.getParameter("employeeName");
+//        empName = "Mr. "+ empName;
+//
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " - udemy instructor");
+//
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model) {
-        String empName=request.getParameter("employeeName");
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
         empName = "Mr. "+ empName;
 
         model.addAttribute("nameAttribute", empName);
