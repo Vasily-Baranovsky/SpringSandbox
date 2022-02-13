7610287bb44a99d2fb5a2209acbe1ccb00f5131e
vabarano <Vasily.Baranovsky@t-systems.com>
2022-02-13 12:58:51 +0100

    Chapter 6. Spring REST. Lection 82 - получение одного работника по id

--- spring_course_rest/src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ spring_course_rest/src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.zaurtregulov.spring.rest.entity.Employee;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class MyRESTController {
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
     }
 }
--- spring_course_rest/src/main/webapp/index.jsp
+++ spring_course_rest/src/main/webapp/index.jsp
@@ -3,6 +3,8 @@
 <h2>Spring REST</h2>
 
 <a href="/spring_course_rest/api/employees">Show all employees</a>
+<br/><br/>
+<a href="/spring_course_rest/api/employees/1">Show employee with id 1</a>
 <br/>
 </body>
 </html>
